c8b5335002f6326158fcf99a577662c0b00b8030
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-08 13:08:24 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
+#include <stdio.h>
 
+/**
+ * main - main function
+ *
+ * return: 0 on success
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle,\n");
 	return (0);
 }
