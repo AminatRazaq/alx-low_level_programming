f760425fcbe5c7cebf92e27f085b023ec6a4b242
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-09 19:01:25 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0 (Succes)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n <= 9)
+		{
+			putchar(n + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
