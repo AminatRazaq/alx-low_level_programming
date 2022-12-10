d164531bb5ea39d70141e04f5a3cc3bba472c07f
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-09 18:43:34 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0 (Succes)
+ *
+ */
+int main(void)
+{
+	int ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
