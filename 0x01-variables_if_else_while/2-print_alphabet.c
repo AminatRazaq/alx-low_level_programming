13f12fe6309dd0e8ab91e315614f29378b67e372
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-09 16:12:00 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0 (Succes)
+ *
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
