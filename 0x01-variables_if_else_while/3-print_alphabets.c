fd57f8d3887d19ded152ce193c4aa4a1f60a7065
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-09 16:20:47 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0 (Succes)
+ *
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch >= 'Z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
