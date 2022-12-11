61e4b3afcf0ae89b116eef0237b7cbc4ed4b6d1a
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-11 15:29:51 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,9 +19,9 @@ int main(void)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) || 
-							(tens < ones) || (hund
-								reds < tens)))
+				if (!((ones == tens) || (tens == hundreds) ||
+							(tens > ones) ||
+							(hundreds > tens)))
 				{
 					putchar(hundreds);
 					putchar(tens);
