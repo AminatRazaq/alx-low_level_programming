4cb10bd47395e57d1a6e28707614b72b4fa67490
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-13 15:45:38 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int _islower(int c);
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
