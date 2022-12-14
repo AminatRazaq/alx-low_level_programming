0adbc24e6816dfb84fe9bafb909597b767e54b82
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-13 17:50:07 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  add - check the code
+ *
+ * @a @b: characters to be checked
+ *
+ * Return: a+b
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
