d60ff4c86b6ab64127475f0d178c17aa77ed483d
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-13 17:15:58 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,7 +14,7 @@ int print_last_digit(int digs)
 {
 	int last_digs = digs % 10;
 
-	last_digs = last_digs;
+	last_digs = last_digs < 0 ? -1 * last_digs : last_digs;
 	_putchar(last_digs + '0');
 
 	return (last_digs < 0 ? -1 * last_digs : last_digs);
