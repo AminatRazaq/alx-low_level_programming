9b61b421eb55d862f340d35c02d1678a6adbb098
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-13 17:10:08 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ *
+ * @digs: number to be checked
+ *
+ * Return: last digit of @digs
+ *
+ */
+
+int print_last_digit(int digs)
+{
+	int last_digs = digs % 10;
+
+	last_digs = last_digs;
+	_putchar(last_digs + '0');
+
+	return (last_digs < 0 ? -1 * last_digs : last_digs);
+}
