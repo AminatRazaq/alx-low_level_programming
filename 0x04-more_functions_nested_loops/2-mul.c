09531e4fc41a5269b427bfb55d6ebc2fa93eb302
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-15 15:45:54 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the uppercase character
+ *
+ * @a: digit (first)
+ * @b: digit (second)
+ *
+ * Return: 0 on success
+ *
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (0);
+}
