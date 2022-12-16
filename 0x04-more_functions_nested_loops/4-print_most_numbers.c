893f5488afc2f2bcbcb922f636ea254d7d714348
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-15 16:04:48 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print 0-9 except 2 and 4
+ *
+ *
+ * Return: 0 on success
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a != 50 || a != 52)
+			_putchar(a);
+	}
+	_putchar('\n');
+}
