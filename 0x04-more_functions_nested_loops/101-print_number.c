f3cb48f49a69fdd4a7d0e63cc99590a2ad7e2d89
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-16 15:24:24 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,12 +15,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('_');
+		_putchar('-');
 		num = -num;
 	}
 	if ((num / 10) > 0)/*numbers greater than 10*/
 	{
-		print_number(num);
+		print_number(num / 10);/*reads all except last digit8*/
 	}
 	_putchar((num % 10) + '0');
 }
