1f6a12d8bc29fec4aa94f8cd21d796b7728916df
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-15 20:53:18 -0800

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - print triangles
+ * @n: number of times _ is done
+ *
+ * Return: 0 on success
+ *
+ */
+
+void print_number(int n)
+{
+	printf("%d", n);
+}
