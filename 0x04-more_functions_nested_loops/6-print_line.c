9d2462909109a7f036870e0f22c30753b02e7625
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-15 18:07:29 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,7 @@
 
 /**
  * print_line - print 0-9 except 2 and 4
- *
+ * @n: number of times _ is done
  *
  * Return: 0 on success
  *
@@ -11,14 +11,15 @@
 
 void print_line(int n);
 {
-	int a = 0;
+	int a = 0;/* number of iterations*/
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		while (a > n)
+		while (a < n)
 		{
 			_putchar('_');
 			a++;
