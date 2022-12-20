8ded2b84713d432c4970d84ff7476d22620affbb
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-19 17:07:51 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - check the code
+ *
+ * @n: the integer declared
+ * return: 0 on success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
