1966410fff2184b68b8d2a7532106f5168806a95
AminatRazaq <aminatrazaq@yahoo.com>
2022-12-19 19:25:26 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,6 +3,28 @@
 
 /**
  * *_strcpy - copies the string pointed to by src
+ * @dest: destinatio of atring
+ * @src: string been copied
  *
+ * Return: return the copied string
  */
 char *_strcpy(char *dest, char *src)
+{
+	int count = 0, i;
+
+	while (*src)
+	{
+		src++;
+		count++;
+	}
+	for (i = 0; i < count; i++)
+		src--;
+	for (i = 0; i < count; i++)
+	{
+		dest[i] = *src;
+		src++;
+	}
+	dest[i] = *src;
+
+	return (dest);
+}
