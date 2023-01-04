7f874926cfc4d4cb95bfacd632bc13dfc1bd88b5
AminatRazaq <aminatrazaq@yahoo.com>
2023-01-04 13:16:30 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,13 +7,11 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i;
+	int i = 0;
 
 	if (*s != '\0')
 	{
-		i = 0;
 		i++;
-		i = _strlen_recursion(s + 1);
+		i += _strlen_recursion(s + 1);
 	}
-	return (i);
 }
