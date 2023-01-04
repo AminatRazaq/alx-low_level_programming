f0a4699ca555aa6c9013589cdff265cff7c35086
AminatRazaq <aminatrazaq@yahoo.com>
2023-01-04 13:08:11 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of a string
+ * @s: character
+ * Return: nothing
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	if (*s != '\0')
+	{
+		i = 0;
+		i++;
+	}
+	return (i);
+}
