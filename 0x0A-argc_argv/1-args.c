24a022bfd808ac44280fda7f288db5d140048aac
AminatRazaq <aminatrazaq@yahoo.com>
2023-01-06 14:36:16 -0800

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: upon success 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;/*because it wasn't used here*/
+	printf("%d\n", argc);
+	return (0);
+}
