e72aa88a330b4a5a150b727bcf96938ec2555c6d
AminatRazaq <aminatrazaq@yahoo.com>
2023-01-06 15:25:33 -0800

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - multiplies two numbers
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: upon success 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;/*first number*/
+	int b;/*second number*/
+
+	if (argc == 3)/*3 arg.i.e filename, first num, second num*/
+	{
+		a = atoi(argv[1]);/*atoi =change string to integer*/
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+		return (o);
+	}
+	printf("Error\n");
+	return (0);
+}
