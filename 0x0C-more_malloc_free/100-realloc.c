9273bd13c537ee2311f0760f2967374893a1c91b
AminatRazaq <aminatrazaq@yahoo.com>
2023-01-12 16:38:16 -0800

    Task 3

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _realloc: reallocates memory
+ * @old_size: old size
+ * @new_size: new size
+ * Return: the pointer
+ */
+void
+{
+	char *clone, *realloc;
+	unsigned int i;
+
+	if (ptr != NULL)
+		clone = ptr;
+	else
+		return (malloc(new-size));
+	if (new_size == old-size)
+		return (ptr);
+
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,12 +13,12 @@ int *array_range(int min, int max)
 	int *arr, i = 0, t = min;
 
 	if (min > max)
-		return (NULL);
+		return (0);
 	arr = malloc((max - min + 1) * sizeof(int));
 
 	if (!arr)
-		return (NULL);
+		return (0);
 	while (i <= max - min)/*max and min is included*/
-		arr[i] = t++;
+		arr[i++] = t++;
 	return (arr);
 }
