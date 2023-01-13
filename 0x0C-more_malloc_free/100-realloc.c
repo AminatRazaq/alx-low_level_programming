c9278b3dc2de0a30b9d515359cabaab3ede80f45
AminatRazaq <aminatrazaq@yahoo.com>
2023-01-12 16:52:22 -0800

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,20 +3,37 @@
 #include "main.h"
 
 /**
- * _realloc: reallocates memory
+ * _realloc - reallocates memory
+ * @ptr: pointer
  * @old_size: old size
  * @new_size: new size
  * Return: the pointer
  */
-void
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, *realloc;
+	char *clone, *reloc;
 	unsigned int i;
 
 	if (ptr != NULL)
 		clone = ptr;
 	else
-		return (malloc(new-size));
-	if (new_size == old-size)
+		return (malloc(new_size));
+	if (new_size == old_size)
 		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (0);
+	}
+	reloc = malloc(new_size);
+	if (reloc == NULL)
+		return (0);
+
+	for (i = 0; i < (old_size || i < new_size); i++)
+	{
+		*(reloc + i) = clone[i];
+	}
+	free(ptr);
+	return (reloc);
+}
 
