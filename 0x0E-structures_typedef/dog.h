f68f4aca504cd5ccd97f9eba53ba80a159f39037
AminatRazaq <aminatrazaq@yahoo.com>
2023-01-16 11:19:25 -0800

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: the struct of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+/*we use dog.h as the header file, so we include all the prototypes*/
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
