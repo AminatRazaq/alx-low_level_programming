1cd215a061383c85492cdb3502c58ecb6227686e
AminatRazaq <aminatrazaq@yahoo.com>
2023-01-26 14:29:58 -0800

    update 8

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * hare_tortoise - prints a string before the main function is excuted
+ */
+void hare_tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
