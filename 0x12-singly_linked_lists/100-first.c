b7e596bf4885785bbe703361ac184ea718951222
AminatRazaq <aminatrazaq@yahoo.com>
2023-01-26 14:57:38 -0800

    update 11

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) hare(void);
+void __attribute__((constructor)) hare_tortoise(void);
 /*__attribute__((constructor)):execute a function when the program starts*/
 /**
  * hare_tortoise - prints a string before the main function is excuted
