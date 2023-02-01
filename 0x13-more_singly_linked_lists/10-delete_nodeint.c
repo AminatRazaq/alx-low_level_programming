676a49e94869ec046da414395322a647428c8ebc
AminatRazaq <aminatrazaq@yahoo.com>
2023-01-31 17:04:32 -0800

    upd 17

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,6 +15,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	/*see question 9 for additional notes*/
 	unsigned int node;
 
+	if (copy == NULL)
+		return (-1);
 	if (index == 0)
 	{
 		*head = (*head)->next;
