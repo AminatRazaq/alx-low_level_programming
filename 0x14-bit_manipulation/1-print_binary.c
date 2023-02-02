7b22f7a195ed7d1318c21f28353445d5b4bd721f
AminatRazaq <aminatrazaq@yahoo.com>
2023-02-02 08:59:37 -0800

    w6

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,4 +2,14 @@
 
 /**
  * print_binary - prints the binary representation of a number
+ * @n: the number to be printed in binary
  *
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar9((n & 1) + '0');
+}
