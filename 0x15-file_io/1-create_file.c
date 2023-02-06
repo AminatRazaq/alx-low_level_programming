f792dde7d2f12a58afcc66b413092d1091a7c1b9
AminatRazaq <aminatrazaq@yahoo.com>
2023-02-06 14:07:24 -0800

    tr 3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,9 +10,8 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len;
+	int o, w, len = 0;
 
-	len = 0;
 	if (filename == NULL)
 		return (-1);
 
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,3 +2,33 @@
 
 /**
  * append_text_to_file - appends text at the end of a file
+ * @filename: the name of the file to create
+ * @text_content:  a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	/*if text_content = NULL, an empty file is created*/
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)/*does not open or write*/
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
+
