4b70de730b1f8089cdf0f22efe685b7c28436dfc
AminatRazaq <aminatrazaq@yahoo.com>
2023-02-06 15:17:34 -0800

    tr 7

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ char *create_buffer(char *file)
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", filei);
+				"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
