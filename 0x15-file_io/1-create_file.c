82726d2851e74a7d406a7e93b8f94d9f5f0e7535
AminatRazaq <aminatrazaq@yahoo.com>
2023-02-06 13:50:44 -0800

    tr 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 	/*if text_content = NULL, an empty file is created*/
-	o = open(filename, O_CREAT || O_RDWR || O_TRUNC, 0000);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0000);
 	w = write(o, text_content, len);
 
 	if (r == -1 || w == -1)/*does not open or write*/
